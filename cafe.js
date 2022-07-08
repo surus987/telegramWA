e35d491141a2fdd37aa9a3aff3edbe61dd8ecc93
surus987 <97225597+surus987@users.noreply.github.com>
2022-07-08 12:36:03 +0300

    Update cafe.js

--- cafe.js
+++ cafe.js
@@ -173,7 +173,7 @@ var Cafe = {
       } else {
         mainButton.setParams({
           is_visible: !!Cafe.canPay,
-          text: 'PAY ' + Cafe.formatPrice(Cafe.totalPrice),
+          text: 'CONFIRM ' + Cafe.formatPrice(Cafe.totalPrice),
           color: '#31b545'
         }).hideProgress();
       }
